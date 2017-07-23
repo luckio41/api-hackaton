c9de40ea067f10b284c360ce3595ee13cce4ee1f
Desarrollo BioBioChile <emunoz.ccp@gmail.com>
2017-07-22 20:40:23 -0400

    votes with filters

---
+++ prensa/app/controllers/VotesController.scala
@@ -0,0 +1,63 @@
+package controllers
+
+import javax.inject._
+
+import models.Vote
+import models.DiputadoNoId
+import play.api.libs.json._
+import play.api.mvc._
+import play.modules.reactivemongo._
+import reactivemongo.api.ReadPreference
+import reactivemongo.play.json._
+import reactivemongo.play.json.collection._
+
+import scala.concurrent.{ExecutionContext, Future}
+
+/**
+  * Created by luckio on 17-05-17.
+  */
+@Singleton
+class VotesController @Inject()(val reactiveMongoApi: ReactiveMongoApi)(implicit exec: ExecutionContext) extends Controller with MongoController with ReactiveMongoComponents {
+
+  def postsFuture: Future[JSONCollection] = database.map(_.collection[JSONCollection]("votos"))
+
+  def index() = Action.async {
+    val futurePostsList: Future[List[Vote]] = postsFuture.flatMap { _
+        .find(Json.obj())
+        .sort(Json.obj("created" -> -1))
+        .cursor[Vote](ReadPreference.primary)
+        .collect[List]()
+    }
+
+    futurePostsList.map { diputados =>
+      Ok(Json.toJson(diputados))
+    }
+  }
+
+  def all() = Action.async {
+    val futurePostsList: Future[List[Vote]] = postsFuture.flatMap { _
+        .find(Json.obj())
+        .sort(Json.obj("created" -> -1))
+        .cursor[Vote](ReadPreference.primary)
+        .collect[List]()
+    }
+
+    futurePostsList.map { diputados =>
+      Ok("callback("+Json.toJson(diputados)+");").as("application/javascript");
+    }
+  }
+
+  def findByDiputadoId(id: Int) = Action.async {
+    val futurePostsList: Future[List[Vote]] = postsFuture.flatMap { _
+        .find(Json.obj("diputado_id" -> id))
+        .sort(Json.obj("created" -> -1))
+        .cursor[Vote](ReadPreference.primary)
+        .collect[List]()
+    }
+
+    futurePostsList.map { diputados =>
+      Ok("callback("+Json.toJson(diputados)+");").as("application/javascript");
+    }
+  }
+
+}
---
+++ prensa/app/models/Vote.scala
@@ -0,0 +1,23 @@
+package models
+
+import play.api.libs.json.Json
+
+/**
+  * Created by luckio on 15-05-17.
+  */
+case class Vote (vote: String, 
+                    diputado_id: Int,
+                    votacion_id: Int,
+                    motivo_ausencia: String,
+                    nombre: String,
+                    comite: String,
+                    img_url: String,
+                    fecha_namcimiento: String,
+                    profesion: String,
+                    region: String,
+                    distrito: Int,
+                    partido: String)
+
+object Vote {
+  implicit val formatter = Json.format[Vote]
+}
